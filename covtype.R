bd310d5aa2500c0be4fd518c6be570a3321141bc
hugulas <hugulas@163.com>
2015-04-09 18:45:38 +0800

    add rpart
    add rpart code

--- covtype.R
+++ covtype.R
@@ -15,13 +15,24 @@ pkgTest("party");
 pkgTest("partykit");
 pkgTest("C50");
 pkgTest("tree");
+pkgTest("rpart");
 covtype<-read.table("covtype.data", header = FALSE,sep = ",");
 x<-covtype[,1:length(names(covtype))-1];
 y<-covtype[,length(names(covtype))];
+#C50 
 covModel<-C50::C5.0(x,factor(y));
 c50result<-summary(covModel);
 c50result;
+#CART
 covModel2<-tree(V55~.,covtype);
 covModel2;
 cartresult<-summary(covModel2);
 cartresult;
+#CTREE error result?
+covModel3<-ctree(V55~.,covtype);
+print(covModel3);
+ctreeesult<-summary(covModel3);
+ctreeesult;
+#RPART
+covModel4<-rpart(V55~.,covtype,method="class");
+print(covModel4);
